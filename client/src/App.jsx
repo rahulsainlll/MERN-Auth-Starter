7df9924fbf865ed2038652ca804eb75ff9c64ee2
rahulsainlll <rahulsain.social777@gmail.com>
2024-02-11 16:48:57 +0530

    Client
    Frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { Routes, Route } from "react-router-dom";
+import axios from "axios";
+import { Toaster } from "react-hot-toast";
+import { UserContextProvider } from "../context/userContext";
+import Dashboard from "./pages/Dashboard";
+
+// Axios
+axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.withCredentials = true;
+
+function App() {
+  return (
+    <UserContextProvider>
+      <Navbar />
+      <Toaster position="top-center" toastOptions={{ duration: 2000 }} />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
+    </UserContextProvider>
+  );
+}
+
+export default App;
